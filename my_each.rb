433b82645b36f02c7b1bb08162b9b0c553204bdb
Joann Pan <joannlpan@gmail.com>
2018-07-23 01:15:39 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  i = 0 
+  while array.length > 4
+    yield(array[i])
+    i = i + 1
+  end
+end
+
+[1, 2, 3, 4].each do |number|
+  puts number
 end
